4f22513321c87ba94d937920d1b747c408ac34e1
chaitanya <saisrikrishna.j@prodapt.com>
2023-09-08 22:44:56 +0530

    no change

--- src/main/java/com/learning/spring/controllers/ClassroomController.java
+++ src/main/java/com/learning/spring/controllers/ClassroomController.java
@@ -1,8 +1,6 @@
 package com.learning.spring.controllers;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.learning.spring.models.ClassroomService;
@@ -24,7 +21,7 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/classroom")
 public class ClassroomController {
-
+    
     @Autowired
     private ClassroomService classroom;
 
@@ -35,33 +32,18 @@ public class ClassroomController {
     }
 
     @PostMapping("/add")
-    @ResponseBody
-    public Map<String, Object> add(@Valid @ModelAttribute Student student, BindingResult result,
-            RedirectAttributes attr) {
-        Map<String, Object> response = new HashMap<>();
-        // Add your validation logic here if needed
-        if (student == null || student.getName() == null) {
-            response.put("success", false);
-            response.put("message", "Invalid student data.");
-            return response;
+    public String add(@Valid @ModelAttribute Student student,BindingResult result, RedirectAttributes attr) {
+        if (result.hasErrors()) {
+            attr.addFlashAttribute("org.springframework.validation.BindingResult.student", result);
+            attr.addFlashAttribute("student", student);
+            return "redirect:/classroom";
         }
-
-        try {
-            classroom.add(student);
-            System.out.println(student);
-            response.put("success", true);
-            response.put("student", student);
-        } catch (Exception e) {
-            response.put("success", false);
-            response.put("message", "Error adding student.");
-        }
-
-        return response;
+        classroom.add(student);
+        return "redirect:/classroom";
     }
 
     @PostMapping("/update/{id}")
-    public String update(@PathVariable int id, @Valid @ModelAttribute Student student, BindingResult result,
-            RedirectAttributes attr) {
+    public String update(@PathVariable int id, @Valid @ModelAttribute Student student, BindingResult result, RedirectAttributes attr) {
         if (result.hasErrors()) {
             attr.addFlashAttribute("org.springframework.validation.BindingResult.student", result);
             attr.addFlashAttribute("student", student);
